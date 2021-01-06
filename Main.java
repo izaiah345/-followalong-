3f5d9c080c11b0867302fe4ef601e575c65493e2
izaiah365 <izaiah.rodriguez@patelhs.org>
2021-01-06 18:34:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Student s1 = new Student("Ahmari", 10, 95.0, true);
+    Student s2 = new Student("Shabil", 9, 99.0, false);
+    Student s3 = new Student("Victor", 10, 88.0, true);
+
+    s1.fieldTrip();
+    s2.fieldTrip();
+    s3.fieldTrip();
+
+   Scanner scan = new Scanner(System.in);
+
+   System.out.println("What is your name?");
+   String name = scan.next();
+   System.out.println("What is your grade level?");
+   int grade = scan.nextInt();
+   System.out.println("What is your avarage grade?");
+   double aveGrade = scan.nextDouble();
+   System.out.println("Do you have signed permission slip? true/false");
+   boolean permissionSlip = scan.nextBoolean(); 
+
+   Student s4 = new Student(name, grade, aveGrade, permissionSlip);
+  }
+}
---
+++ Student.java
@@ -0,0 +1,25 @@
+public class Student{
+ //construdctor 
+ private String name;
+ private int gradeYear;
+ private double aveGrade;
+ private boolean permissionSlip; 
+
+ //variable 
+ public Student(String studentName, int studentYear, double studentGrade,boolean permission){
+   name = studentName;
+   gradeYear = studentYear;
+   aveGrade = studentGrade;
+   permissionSlip = permission;
+ }
+
+ //method 
+ public void fieldTrip(){
+   if(gradeYear >= 10 && aveGrade > 70.0 && permissionSlip)
+   {
+     System.out.println("Yay you get to go on a field trip, " + name);
+   }else{
+     System.out.println("Sorry, maybe next year.");
+   }
+ } 
+} 
